e6a235290c29df404fad326c27049cf273ad3d64
pc <18909076+pcacho@users.noreply.github.com>
2018-12-19 16:44:51 -0500

    -Use built-in (bsp/env/freedom-e300-hifive/init.c) to configure PLL and UART. -Patch file to disable init.c's debug printf and interrupt routine call

--- pablOS.c
+++ pablOS.c
@@ -29,7 +29,9 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <stdio.h>
 #include <stdint.h>
+#include "encoding.h"
 #include "platform.h"
 #include "uart.h"
 #include "util.h"
@@ -41,20 +43,7 @@
 static char pablOS_version[] = "\a\n\r\n\rpablOS Version 0.1\n\r";
 
 int main(void) {
-	// Make sure the HFROSC is on before the next line:
-	PRCI_REG(PRCI_HFROSCCFG) |= ROSC_EN(1);
-	// Run off 16 MHz Crystal for accuracy. Note that the
-	// first line is
-	PRCI_REG(PRCI_PLLCFG) = (PLL_REFSEL(1) | PLL_BYPASS(1));
-	PRCI_REG(PRCI_PLLCFG) |= (PLL_SEL(1));
-	// Turn off HFROSC to save power
-	PRCI_REG(PRCI_HFROSCCFG) &= ~(ROSC_EN(1));
-
-	// Initialize UART
-	uart_init();
-
-	uart_puts(pablOS_version);
-	uart_puts("\n\r");
+	printf("%s\n\r", pablOS_version);
 	util_memdump((uint32_t*) &pablOS_version[0], 64);
 
 #if defined(TEST_BUILD)
